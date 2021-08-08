1ada0d308c79ad55a76c3125ccee5e356d05e5d8
nguyen phat <nguyenvanphat20999@gmail.com>
2021-08-08 11:12:33 +0700

    testok

--- src/routes/ticketRouter.js
+++ src/routes/ticketRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const ticketController = require("../app/controllers/ticketController");
 
-router.get("/get", ticketController.getTicket);
-router.post("/post", ticketController.postTicket);
-router.delete("/delete/:id", ticketController.deleteTicket);
+router.get("/", ticketController.getTicket);
+router.post("/", ticketController.postTicket);
+router.delete("/:id", ticketController.deleteTicket);
 
 module.exports = router;
