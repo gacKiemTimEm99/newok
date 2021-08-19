58ac0810e9984cba7fef2219cee8cff9c8f84e16
nguyen phat <nguyenvanphat20999@gmail.com>
2021-08-19 23:19:15 +0700

    'm'

--- src/app/controllers/shownControllers.js
+++ src/app/controllers/shownControllers.js
@@ -5,6 +5,10 @@ class shownController {
   config(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header(
       "Access-Control-Allow-Headers",
       "X-Requested-With, Content-Type, Accept"
     );
